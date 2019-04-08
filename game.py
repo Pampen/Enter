7d437e7ad821f8c1b0719af9d0169fdab0d0a9be
Jacky Tu <jacky99@hotmail.se>
2019-04-08 16:36:08 +0200

    Ändrat namn på alla rum

--- game.py
+++ game.py
@@ -26,18 +26,20 @@ def getResponse(userInput, state):
         return handlePorch(userInput, state)
     elif state['level'] == 'MAIN HALL':
         return handleMainHall(userInput, state)
-    elif state['level'] == 'FURY':
-        return handleFury(userInput, state)
-    elif state['level'] == 'ANGER':
-        return handleAnger(userInput, state)
-    elif state['level'] == 'WRATH':
-        return handleWrath(userInput, state)
-    elif state['level'] == 'RESENTMENT':
-        return handleResentment(userInput, state)
-    elif state['level'] == 'BITTERNESS':
-        return handleBitterness(userInput, state)
-    elif state['level'] == 'RAGE':
-        return handleRage(userInput, state)
+    elif state['level'] == 'LIVING ROOM':
+        return handleLivingRoom(userInput, state)
+    elif state['level'] == 'KITCHEN':
+        return handleKitchen(userInput, state)
+    elif state['level'] == 'HALL':
+        return handleHall(userInput, state)
+    elif state['level'] == 'UPPER FLOOR':
+        return handleUpperFloor(userInput, state)
+    elif state['level'] == 'BEDROOM':
+        return handleBedroom(userInput, state)
+    elif state['level'] == 'BASEMENT':
+        return handleBasement(userInput, state)
+    elif state['level'] == 'ATTIC':
+        return handleAttic(userInput, state)
     
 def handleOutside(userInput, state):
     if userInput == 'GO WEST':
@@ -71,66 +73,76 @@ def handleMainHall(userInput, state):
     if userInput == 'GO SOUTH':
         return goToLevel(state, 'PORCH')
     elif userInput == 'GO NORTH':
-        return goToLevel (state, 'FURY')
+        return goToLevel (state, 'LIVING ROOM')
     elif userInput == 'GO WEST' or  userInput == 'GO EAST':
         return handleInvalidDirection(state)
     else:
         return handleInvalidInput(userInput, state)
 
-def handleFury(userInput, state):
+def handleLivingRoom(userInput, state):
     if userInput == 'GO SOUTH':
         return goToLevel(state, 'MAIN HALL')
     elif userInput == 'GO EAST':
-        return goToLevel(state, 'ANGER')
+        return goToLevel(state, 'KITCHEN')
     elif userInput == 'GO WEST':
-        return goToLevel(state, 'WRATH')
+        return goToLevel(state, 'HALL')
     elif userInput == 'GO NORTH' or userInput == 'GO WEST':
         return handleInvalidDirection(state)
     else:
         return handleInvalidInput(userInput, state)
 
-def handleAnger(userInput, state):
+def handleKitchen(userInput, state):
     if userInput == 'GO WEST':
-        return goToLevel(state, 'FURY')
+        return goToLevel(state, 'LIVING ROOM')
     elif userInput == 'GO SOUTH':
-        return goToLevel(state, 'RAGE')
+        return goToLevel(state, 'BASEMENT')
     elif userInput == 'GO NORTH' or userInput == 'GO EAST':
         return handleInvalidDirection(state)
     else:
         return handleInvalidInput(userInput, state)
 
-def handleWrath(userInput, state):
+def handleHall(userInput, state):
     if userInput == 'GO EAST':
-        return goToLevel(state, 'FURY')
+        return goToLevel(state, 'LIVING ROOM')
     elif userInput == 'GO NORTH':
-        return goToLevel(state, 'RESENTMENT')
+        return goToLevel(state, 'UPPER FLOOR')
     elif userInput == 'GO SOUTH' or  userInput == 'GO WEST':
         return handleInvalidDirection(state)
     else:
         return handleInvalidInput(userInput, state)
 
-def handleResentment(userInput, state):
+def handleUpperFloor(userInput, state):
     if userInput == 'GO SOUTH':
-        return goToLevel(state, 'WRATH')
+        return goToLevel(state, 'HALL')
     elif userInput == 'GO WEST':
-        return goToLevel(state, 'BITTERNESS')
-    elif userInput == 'GO NORTH' or userInput == 'GO EAST':
+        return goToLevel(state, 'BEDROOM')
+    elif userInput == 'GO NORTH':
+        return goToLevel(state, 'ATTIC')
+    elif userInput == 'GO EAST':
         return handleInvalidDirection(state)
     else:
         return handleInvalidInput(userInput, state)
 
-def handleBitterness(userInput, state):
+def handleAttic(userInput, state):
+    if userInput == 'GO SOUTH':
+        return goToLevel(state, 'UPPER FLOOR')
+    elif userInput == 'GO NORTH' or userInput == 'GO WEST' or userInput == 'GO EAST':
+        return handleInvalidDirection(state)
+    else:
+        return handleInvalidInput(userInput, state)
+
+def handleBedroom(userInput, state):
     if userInput == 'GO EAST':
-        return goToLevel(state, 'RESENTMENT')
+        return goToLevel(state, 'UPPER FLOOR')
     elif userInput == 'GO NORTH' or userInput == 'GO SOUTH' or userInput == 'GO EAST':
         return handleInvalidDirection(state)
     else:
         return handleInvalidInput(userInput, state)
 
-def handleRage(userInput, state):
+def handleBasement(userInput, state):
     if userInput == 'GO NORTH':
-        return goToLevel(state, 'ANGER')
-    elif userInput == 'GO NORTH' or userInput == 'GO SOUTH' or userInput == 'GO EAST':
+        return goToLevel(state, 'KITCHEN')
+    elif userInput == 'GO WEST' or userInput == 'GO SOUTH' or userInput == 'GO EAST':
         return handleInvalidDirection(state)
     else:
         return handleInvalidInput(userInput, state)
--- red.py
+++ red.py
@@ -1,57 +1,57 @@
-def handleFury(userInput, state):
+def handleLivingRoom(userInput, state):
     if userInput == 'GO SOUTH':
         return goToLevel(state, 'MAIN HALL')
     elif userInput == 'GO EAST':
-        return goToLevel(state, 'ANGER')
+        return goToLevel(state, 'KTICHEN')
     elif userInput == 'GO WEST':
-        return goToLevel(state, 'WRATH')
+        return goToLevel(state, 'HALL')
     elif userInput == 'GO NORTH' or userInput == 'GO WEST':
         return handleInvalidDirection(state)
     else:
         return handleInvalidInput(userInput, state)
 
-def handleAnger(userInput, state):
+def handleKitchen(userInput, state):
     if userInput == 'GO WEST':
-        return goToLevel(state, 'FURY')
+        return goToLevel(state, 'LIVING ROOM')
     elif userInput == 'GO SOUTH':
-        return goToLevel(state, 'RAGE')
+        return goToLevel(state, 'BASEMENT')
     elif userInput == 'GO NORTH' or userInput == 'GO EAST':
         return handleInvalidDirection(state)
     else:
         return handleInvalidInput(userInput, state)
 
-def handleWrath(userInput, state):
+def handleHall(userInput, state):
     if userInput == 'GO EAST':
-        return goToLevel(state, 'FURY')
+        return goToLevel(state, 'LIVING ROOM')
     elif userInput == 'GO NORTH':
-        return goToLevel(state, 'RESENTMENT')
+        return goToLevel(state, 'UPPER FLOOR')
     elif userInput == 'GO SOUTH' or  userInput == 'GO WEST':
         return handleInvalidDirection(state)
     else:
         return handleInvalidInput(userInput, state)
 
-def handleResentment(userInput, state):
+def handleUpperFloor(userInput, state):
     if userInput == 'GO SOUTH':
-        return goToLevel(state, 'WRATH')
+        return goToLevel(state, 'HALL')
     elif userInput == 'GO WEST':
-        return goToLevel(state, 'BITTERNESS')
+        return goToLevel(state, 'ATTIC')
     elif userInput == 'GO NORTH' or userInput == 'GO EAST':
         return handleInvalidDirection(state)
     else:
         return handleInvalidInput(userInput, state)
 
-def handleBitterness(userInput, state):
+def handleAttic(userInput, state):
     if userInput == 'GO EAST':
-        return goToLevel(state, 'RESENTMENT')
+        return goToLevel(state, 'UPPER FLOOR')
     elif userInput == 'GO NORTH' or userInput == 'GO SOUTH' or userInput == 'GO EAST':
         return handleInvalidDirection(state)
     else:
         return handleInvalidInput(userInput, state)
 
-def handleRage(userInput, state):
+def handleBasement(userInput, state):
     if userInput == 'GO NORTH':
-        return goToLevel(state, 'ANGER')
-    elif userInput == 'GO NORTH' or userInput == 'GO SOUTH' or userInput == 'GO EAST':
+        return goToLevel(state, 'KITCHEN')
+    elif userInput == 'GO WEST' or userInput == 'GO SOUTH' or userInput == 'GO EAST':
         return handleInvalidDirection(state)
     else:
         return handleInvalidInput(userInput, state)
