2136dfd1a348e789f4d31eb9b9b035f604fd30e8
Felicia Lindbäck <felicia.lindback@outlook.com>
2019-04-10 13:28:04 +0200

    CSS grejer has happened.

--- src/App.js
+++ src/App.js
@@ -46,41 +46,46 @@ class App extends Component {
 
   render() {
     return (
-      <div id="wrapper">
-        <main>
-          <div className="container">
-            <div className="game-container">
-              <div className="game-screen">
+      <main id="wrapper">
+        <div className="container">
+          <div className="button-container">
+            <div id="main-buttons">
+              <button className="game-button" id="inventory-button">I</button>
+              <button className="game-button" id="map-button">M</button>
+            </div>
+            <button className="game-button" id="command-button">C</button>
+          </div>
+          <div className="game-container">
+            <div className="game-screen">
               <div className="game-screen-header">
                 <h2 id="title">{this.state.title}</h2>
                 <p className="level-description">{this.state.description}</p>
               </div>
-                <div className="game-text-box">
-                  {
-                    this.state.chatboxText.map((text, i) => {
-                      return <p key={i} className="chatbox-text">{text}</p>
-                    })
-                  }
-                </div>
-              </div>
-              <div className="terminal">
-                <form onSubmit={this.handleClick}>
-                  <input 
-                      ref={
-                        function(inputElement) {
-                          this.inputElement=inputElement;
-                        }.bind(this)
-                      }
-                      type="text"
-                      className="terminal-input"
-                  ></input>
-                  <button id="game-button">Enter</button>
-                </form>
+              <div className="game-text-box">
+                {
+                  this.state.chatboxText.map((text, i) => {
+                    return <p key={i} className="chatbox-text">{text}</p>
+                  })
+                }
               </div>
             </div>
+            <div className="terminal">
+              <form onSubmit={this.handleClick}>
+                <input 
+                    ref={
+                      function(inputElement) {
+                        this.inputElement=inputElement;
+                      }.bind(this)
+                    }
+                    type="text"
+                    className="terminal-input"
+                ></input>
+                <button id="game-button">Enter</button>
+              </form>
+            </div>
           </div>
-        </main>
-      </div>
+        </div>
+      </main>
     );
   }
 }
