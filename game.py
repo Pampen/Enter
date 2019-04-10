b42d54d917ff51750c448d2fac64d7b85b14736b
KalleHamnebo <kallebertilsson@hotmail.com>
2019-04-10 13:45:25 +0200

    Pink module

--- game.py
+++ game.py
@@ -25,6 +25,19 @@ def getResponse(userInput, state):
         return handleShed(userInput, state)
     elif state['level'] == 'PORCH':
         return handlePorch(userInput, state)
+    elif state['level'] == 'MAIN HALL':
+        return handleMainHall(userInput, state)
+    elif state['level'] == 'CRIBROOM':
+        return handleCribroom(userInput, state)
+    elif state['level'] == 'BABYROOM':
+        return handleBabyroom(userInput, state)
+    elif state['level'] == 'NURSINGROOM':
+        return handleNursingroom(userInput, state)
+    elif state['level'] == 'STUDYROOM':
+        return handleStudyroom(userInput, state)
+    elif state['level'] == 'MESSYROOM':
+        return handleMessyroom(userInput, state)
+    
     
 def handleOutside(userInput, state):
     if userInput == 'GO WEST':
@@ -39,7 +52,19 @@ def handleOutside(userInput, state):
 def handlePorch(userInput, state):
     if userInput == 'GO SOUTH':
         return goToLevel(state, 'OUTSIDE')
-    elif userInput == 'GO NORTH' or userInput == 'GO EAST' or userInput == 'GO WEST':
+    if userInput == 'GO EAST':
+        return handleInvalidDirection(state)
+    if userInput == 'GO NORTH':
+        return goToLevel(state, 'MAIN HALL')
+    elif userInput == 'GO WEST':
+        return handleInvalidDirection(state)
+    else:
+        return handleInvalidInput(userInput, state)
+
+def handleMainHall(userInput, state):
+    if userInput == 'GO EAST':
+        return goToLevel(state, 'CRIBROOM')
+    elif userInput == 'GO NORTH' or userInput == 'GO SOUTH' or userInput == 'GO WEST':
         return handleInvalidDirection(state)
     else:
         return handleInvalidInput(userInput, state)
@@ -51,6 +76,50 @@ def handleShed(userInput, state):
         return handleInvalidDirection(state)
     else:
         return handleInvalidInput(userInput, state)
+
+def handleCribroom(userInput, state):
+    if userInput == 'GO NORTH':
+        return goToLevel(state, 'BABYROOM')
+    elif userInput == 'GO EAST':
+        return goToLevel(state, 'MESSYROOM')
+    elif userInput == 'GO WEST':
+        return goToLevel(state, 'MAIN HALL')
+    elif userInput == 'GO SOUTH':
+        return handleInvalidDirection(state)
+    else:
+        return handleInvalidInput(userInput, state)
+
+def handleBabyroom(userInput, state):
+    if userInput == 'GO EAST':
+        return goToLevel(state, 'NURSINGROOM')
+    elif userInput == 'GO SOUTH':
+        return goToLevel(state, 'CRIBROOM')
+    elif userInput == 'GO NORTH' or userInput == 'GO WEST':
+        return handleInvalidDirection(state)
+
+def handleNursingroom(userInput, state):
+    if userInput == 'GO WEST':
+        return goToLevel(state, 'BABYROOM')
+    if userInput == 'GO SOUTH':
+        return goToLevel(state, 'STUDYROOM')
+    elif userInput == 'GO NORTH' or userInput == 'GO EAST':
+        return handleInvalidDirection(state)
+
+def handleStudyroom(userInput, state):
+    if userInput == 'GO NORTH':
+        return goToLevel(state, 'NURSINGROOM')
+    if userInput == 'GO SOUTH':
+        return goToLevel(state, 'MESSYROOM')
+    elif userInput == 'GO WEST' or userInput == 'GO EAST':
+        return handleInvalidDirection(state)
+
+def handleMessyroom(userInput, state):
+    if userInput == 'GO NORTH':
+        return goToLevel(state, 'STUDYROOM')
+    if userInput == 'GO WEST':
+        return goToLevel(state, 'CRIBROOM')
+    elif userInput == 'GO EAST' or userInput == 'GO SOUTH':
+        return handleInvalidDirection(state)
 
 def goToLevel(state, currentLevel):
     response = {
