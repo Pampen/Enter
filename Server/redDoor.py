9c194b6e04a1c4ef7a9a85c3c43231de0edc3a67
Jacky Tu <jacky99@hotmail.se>
2019-04-30 22:37:25 +0200

    Fixing the last puzzle

--- Server/redDoor.py
+++ Server/redDoor.py
@@ -6,10 +6,16 @@ def handleLivingRoom(userInput, state):
     elif userInput == 'TAKE RED KEY':
         return takeItem(state, 'redKey')
     elif userInput == 'GO EAST':
-        return goToLevel(state, 'KITCHEN', userInput)
+        if 'photograph' in state['inventory']: 
+            return goToLevel(state, 'KITCHEN', userInput) and handleNewKitchenDesc(state)
+        else:
+            return goToLevel(state, 'KITCHEN', userInput)
     elif userInput == 'GO WEST':
-        return goToLevel(state, 'HALL', userInput)
-    elif userInput == 'GO NORTH' or userInput == 'GO WEST':
+        if 'carKeys' in state['inventory']: 
+            return goToLevel(state, 'HALL', userInput) and handleNewHallDesc(state)
+        else:
+            return goToLevel(state, 'HALL', userInput)
+    elif userInput == 'GO NORTH':
         return handleInvalidDirection(state)
     else:
         return handleInvalidInput(userInput, state)
@@ -17,7 +23,7 @@ def handleLivingRoom(userInput, state):
 def handleKitchen(userInput, state):
     if userInput == 'GO WEST':
         if 'photograph' and 'carKeys' and 'canvas' in state['inventory']: 
-            return goToLevel(state, 'LIVING_ROOM', userInput) and handleNewLivingRoomdDesc(state)
+            return goToLevel(state, 'LIVING_ROOM', userInput) and handleNewLivingRoomDesc(state)
         else:
             return goToLevel(state, 'LIVING_ROOM', userInput)
     elif userInput == 'TAKE PHOTOGRAPH':
@@ -32,7 +38,7 @@ def handleKitchen(userInput, state):
 def handleHall(userInput, state):
     if userInput == 'GO EAST':
         if 'photograph' and 'carKeys' and 'canvas' in state['inventory']: 
-            return goToLevel(state, 'LIVING_ROOM', userInput) and handleNewLivingRoomdDesc(state)
+            return goToLevel(state, 'LIVING_ROOM', userInput) and handleNewLivingRoomDesc(state)
         else:
             return goToLevel(state, 'LIVING_ROOM', userInput)
     elif userInput == 'TAKE CAR KEYS':
@@ -46,7 +52,10 @@ def handleHall(userInput, state):
 
 def handleUpperFloor(userInput, state):
     if userInput == 'TAKE STAIRS' or userInput == 'USE STAIRS':
-        return goToLevel(state, 'HALL', userInput)
+        if 'carKeys' in state['inventory']: 
+            return goToLevel(state, 'HALL', userInput) and handleNewHallDesc(state)
+        else:
+            return goToLevel(state, 'HALL', userInput)
     elif userInput == 'USE GREY KEY' and "greyKey" in state["inventory"]:
         return goToLevel(state, 'BEDROOM', userInput)
     elif userInput == 'GO WEST':
@@ -84,7 +93,10 @@ def handleBedroom(userInput, state):
 
 def handleBasement(userInput, state):
     if userInput == 'TAKE STAIRS' or userInput == 'USE STAIRS':
-        return goToLevel(state, 'KITCHEN', userInput)
+        if 'photograph' in state['inventory']: 
+            return goToLevel(state, 'KITCHEN', userInput) and handleNewKitchenDesc(state)
+        else:
+            return goToLevel(state, 'KITCHEN', userInput)
     elif userInput == 'TAKE LADDER':
         return takeItem(state, 'ladder')
     elif userInput == 'GO WEST' or userInput == 'GO SOUTH' or userInput == 'GO EAST':
@@ -92,12 +104,32 @@ def handleBasement(userInput, state):
     else:
         return handleInvalidInput(userInput, state)
 
-def handleNewLivingRoomdDesc(state):
+def handleNewLivingRoomDesc(state):
     response = {
         'state': state,
         'pageChanges': {
             'levelTitle': "Living Room",
             'levelDescription': "With the canvas, car keys and the photograph in your possesion you can feel the heat from the fireplace, some things are meant to be burned down to ashes."
+        }
+    }
+    return response
+
+def handleNewKitchenDesc(state):
+    response = {
+        'state': state,
+        'pageChanges': {
+            'levelTitle': "Kitchen",
+            'levelDescription': "An old kitchen, but some modern modifications. There are some plates and glasses in the sink that needs to be washed. By the looks of it, it seems that the person who lived here hasn't done her or his dishes for a while. There is some stairs leading down to the basement."
+        }
+    }
+    return response
+
+def handleNewHallDesc(state):
+    response = {
+        'state': state,
+        'pageChanges': {
+            'levelTitle': "Hall",
+            'levelDescription': "A small hall with a clothing rack on the side and a small red carpet with the letter 'A' on it. Why the letter 'A'? There are also some pictures and the wall, however, you can't seem to see persons' faces in the pictures for some reasons. Every face is blurry. Beneath the pictures is a small table with an empty bowl. There is some stairs leading up to the upper floor."
         }
     }
     return response
