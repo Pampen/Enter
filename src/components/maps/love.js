ef6f5c0230501abda4e26732debf7545fae736e9
Felicia Lindbäck <felicia.lindback@outlook.com>
2019-05-08 16:48:47 +0200

    Added constants for LOVE module's map

---
+++ src/components/maps/love.js
@@ -0,0 +1,51 @@
+const cribRoom = {
+    color: 'pink',
+    name: 'Crib Room',
+    id: 'CRIB_ROOM',
+    cellName:'CR',
+    connections: [
+        'N', 'E', 'W'
+    ]
+}
+const babyRoom = {
+    color: 'pink',
+    name: 'Baby Room',
+    id: 'BABY_ROOM',
+    cellName: 'BR',
+    connections: [
+        'E', 'S'
+    ]
+}
+const nursingRoom = {
+    color: 'pink',
+    name: 'Nursing Room',
+    id: 'NURSING_ROOM',
+    cellName: 'NR',
+    connections: [
+        'W', 'S', 'E'
+    ] 
+}
+const studyRoom = {
+    color: 'pink',
+    name: 'Study Room',
+    id: 'STUDY_ROOM',
+    cellName: 'SR',
+    connections: [
+        'N', 'S'
+    ]
+}
+const messyRoom = {
+    color: 'pink',
+    name: 'Messy Room',
+    id: 'MESSY_ROOM',
+    cellName: 'MR',
+    connections: [
+        'N', 'W', 'E'
+    ]
+}
+
+const loveMap = [
+    [],
+    [],
+    []
+]
