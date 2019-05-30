12955cb8a2a4210b1b00ba77477889910d66f330
Felicia Lindbäck <felicia.lindback@outlook.com>
2019-05-30 16:47:18 +0200

    Fixed color-changes in Mirror Room.

--- src/components/game-screen.js
+++ src/components/game-screen.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import { levels } from '../utilities/levelChecker'
 
+const mirrorRoomColorMap = {
+    'ANGER_1': 'ANGER',
+    'ANGER_2': 'ANGER',
+    'LOVE': 'LOVE',
+    'SADNESS_1': 'SADNESS',
+    'SADNESS_2': 'SADNESS',
+    'JOY': 'JOY',
+    'MIRROR_ROOM_1': 'TUTORIAL',
+    'MIRROR_ROOM_2': 'TUTORIAL'
+}
+
 export default class GameScreen extends Component {
     render() {
-        const color = levels[this.props.level]
+        const mainLevel =  levels[this.props.level]
+        const color = mainLevel !== 'MIRROR_ROOM'
+            ? mainLevel
+            : mirrorRoomColorMap[this.props.level]
         return (
             <div className="game-screen">
                 <div className="game-screen-header">
